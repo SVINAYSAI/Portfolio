987a12d714946900c86ab887477e8add88e94430
SVINAYSAI <svinaysai6@gmail.com>
2024-02-13 22:58:46 +0530

    svs

--- components/projects.tsx
+++ components/projects.tsx
@@ -22,12 +22,17 @@ type ProjectsProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags }: ProjectsProps) {
   return (
-    <section>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
+    <section className="bg-gray-100 max-w-[42rem] border border-black/[5] owerflow-hidden sm:pr-8">
+      <h3 className="text-2xl font-semibold">{title}</h3>
+      <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+      <ul className="flex flex-wrap mt-4 gap-2">
         {tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
+          <li
+            className="bg-black/[0.7] px-3 py-1 text-[0.7] uppercase tracking-wider text-white rounded-full"
+            key={index}
+          >
+            {tag}
+          </li>
         ))}
       </ul>
     </section>
